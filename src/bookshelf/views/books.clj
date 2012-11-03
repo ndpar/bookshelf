c5b91d5bfd35ddab1cb3d8bda17ee2d178499aa0
Andrey Paramonov <git@ndpar.com>
2012-11-03 17:58:21 -0400

    Book table view

--- src/bookshelf/views/books.clj
+++ src/bookshelf/views/books.clj
@@ -1,7 +1,37 @@
 (ns bookshelf.views.books
   (:require [bookshelf.views.common :as common])
-  (:use [noir.core :only [defpage]]))
+  (:use [noir.core :only [defpage]]
+        [hiccup.element :only [link-to]]))
+
+(def books
+  [{:author "Fogus M., Houser C."
+    :title "The Joy Of Clojure"
+    :year "2011"
+    :format "pdf"
+    :id 1}
+   {:author "Fogus M., Houser C."
+    :title "The Joy Of Clojure"
+    :year "2011"
+    :format "epub"
+    :id 2}])
+
+(defn- list-books []
+  [:table
+   [:thead
+    [:tr
+     [:th "Author"]
+     [:th "Title"]
+     [:th "Published"]
+     [:th "Format"]]]
+   (into [:tbody]
+         (for [book books]
+           [:tr
+            [:td (:author book)]
+            [:td (:title book)]
+            [:td (:year book)]
+            [:td (link-to (clojure.string/join "/" ["/books" (:id book) (:format book)])
+                          (:format book))]]))])
 
 (defpage "/books" []
   (common/layout
-    [:p "Welcome to bookshelf"]))
+    (list-books)))
