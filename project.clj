18829f1ff9205342fd442b97bec1982809d67c68
Andrey Paramonov <git@ndpar.com>
2012-11-07 21:01:01 -0500

    Released version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bookshelf "0.1.0-SNAPSHOT"
+(defproject bookshelf "0.1.0"
   :description "Bookshelf site"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta3"]]
